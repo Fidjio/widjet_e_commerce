9a90eb60243ac051f1606b0124787de657aeac60
Сергей Карсканов <kapckahob97@gmail.com>
2025-04-20 19:15:27 +0300

    add tests for new packs product and category

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,10 +1,33 @@
+from src.product import Product
+
+
 def test_category_init(category_init_1, category_init_2):
     assert category_init_1.name == "Продовольствие"
     assert category_init_1.description == "Товары для питания"
-    assert len(category_init_1.products) == 2
+    assert len(category_init_1.products_list) == 2
 
     assert category_init_1.count_category == 2
     assert category_init_2.count_category == 2
 
     assert category_init_1.count_products == 5
     assert category_init_2.count_products == 5
+
+
+def test_add_product(category_init_1):
+    prod1 = Product("Перчик", "Овощ", 2000.00, 99)
+    category_init_1.add_product(prod1)
+    assert category_init_1.products == (
+                                        'Арбуз, 50.0 руб. Остаток: 3 шт.\n'
+                                        'Картошка, 25.0 руб. Остаток: 2 шт.\n'
+                                        'Перчик, 2000.0 руб. Остаток: 99 шт.\n'
+    )
+
+
+def test_products(category_init_1):
+    assert category_init_1.products == ('Арбуз, 50.0 руб. Остаток: 3 шт.\n'
+                                        'Картошка, 25.0 руб. Остаток: 2 шт.\n')
+
+
+def test_products_list(category_init_1):
+    assert category_init_1.products_list
+    assert len(category_init_1.products_list) == 2
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,32 @@
+import sys
+from io import StringIO
+
+from src.product import Product
+
+
 def test_product_init(product_init):
     assert product_init.name == "Арбуз"
     assert product_init.price == 50.00
     assert product_init.description == "Ягода"
     assert product_init.quantity == 3
+
+
+def test_new_product():
+    prod = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+
+    assert prod
+    assert prod.name == "Samsung Galaxy S23 Ultra"
+    assert prod.description == "256GB, Серый цвет, 200MP камера"
+
+
+def test_price(product_init):
+    assert product_init.price == 50.00
+
+    product_init.price = 100.00
+    assert product_init.price == 100.00
+
+    product_init.price = 0
+    product_init.price = -20.00
+    assert product_init.price == 100.00
