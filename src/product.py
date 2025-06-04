12172fb8fb703f9309b11097510ce83875b0032f
Сергей Карсканов <kapckahob97@gmail.com>
2025-06-04 15:05:37 +0300

    add cls LawnGrass and add func init share

--- src/product.py
+++ src/product.py
@@ -51,3 +51,13 @@ class Smartphone(Product):
         self.model = model
         self.memory = memory
         self.color = color
+
+
+class LawnGrass(Product):
+
+    def __init__(self, name: str, description: str, price: float, quantity: int, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
